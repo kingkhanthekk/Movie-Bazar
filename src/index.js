cafeef88ea1c6207dc8ee3f9f36f85d7e0e83870
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-14 18:52:21 +0300

    Refactored code to work with star rating. Added svg files and initial codes to show start rating.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
+import RatingStar from "./RatingStar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RatingStar />
   </React.StrictMode>
 );
